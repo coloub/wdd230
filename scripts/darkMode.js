9cb1817b6f4250913c1c9d65ac7a5246bfaca3c5
coloub <jose1252jose@gmail.com>
2025-03-12 14:58:27 -0400

    #

---
+++ scripts/darkMode.js
@@ -0,0 +1,11 @@
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        darkModeToggle.textContent = '☀️';
+    } else {
+        darkModeToggle.textContent = '🌙';
+    }
+});
